3dce305fa97b3a1eb481e061d3cb2d898c1974c7
JENMukiri <jmukiri@gmail.com>
2021-12-01 18:43:03 -0800

    new changes to the app

--- app.R
+++ app.R
@@ -1,17 +1,16 @@
 library(shiny)
 library(tidyverse)
 library(shinythemes)
+library(DT)
 bcl <- read_csv("bcl-data.csv")
 options(shiny.autoreload = TRUE)
 #max_price <- max(bcl$Price, na.rm = TRUE)
 
 
-ui <- fluidPage(
+ui <- fluidPage( theme = shinytheme("cosmo"),
     titlePanel("BCL app"),
-    "(Explanation goes here) 
-  This is an app that helps you explore the alcohol
-  content of beverages at BC Liquor stores between
-  two price points.",
+    "This app allows you to explore the bc liqour database and the types of alcoholic and non-alcholic drinks they sell,
+      the alcholo content, price and countries or origin.",
   tags$br(),
   tags$br(),
   
@@ -24,13 +23,15 @@ ui <- fluidPage(
           radioButtons(
               "my_radio", "Select beverage type.", 
               choices = unique(bcl$Type)
+          
           )
+          
       ),
       mainPanel(
         img(src='myImage.png', align = "right"),
         tabsetPanel(
           tabPanel("Plot", uiOutput("my_random")),
-          tabPanel("Table", tableOutput("my_table")),
+          tabPanel("Table", DTOutput("my_table")),
           tabPanel("Download Data",  downloadButton("downloadData"))
          )
       )
@@ -40,6 +41,16 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
     
+  observeEvent(input$column, {
+    updateSelectInput(session, "value", paste("Select", input$column, "value(s)"),
+                      choices = sort(unique(mtcars[[input$column]])))
+  })
+  
+  output$selected <- renderText({
+    paste0(input$column, ": ", paste(input$value, collapse = ", "))
+  })
+  
+  
     output$my_random <- renderUI({
         if (input$my_radio == "WINE") {
             plotOutput("my_plot")
@@ -61,9 +72,9 @@ server <- function(input, output, session) {
             geom_histogram()
     )
     
-    output$my_table <- renderTable(
-        filtered()
-    )
+    output$my_table <- renderDT(
+      filtered(), options = list(lengthChange = FALSE)
+    ) 
     
 #Downloadable csv of selected dataset ----
       output$downloadData <- downloadHandler(
@@ -75,6 +86,15 @@ server <- function(input, output, session) {
         }
       )
   
+    output$image <- renderImage({
+      filename <- normalizePath(file.path(paste0('www/', myImage, '.png')))
+      list(
+        src = filename, 
+        height = 200
+      )
+    }, deleteFile = FALSE)    
+    
+    
 }
 
 
